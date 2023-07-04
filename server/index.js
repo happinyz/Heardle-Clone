f6253d80df64faac9583868687a7745c2a261df5
Alvin Zou <sc.alvinzou@gmail.com>
2023-07-03 22:22:02 -0700

    Vercel config

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const app = express();
 
 const PORT = 5000;
 
-const URL = 'https://p.scdn.co/mp3-preview/cea8b314f8b3777c6b87a45187b7b522d3911fde?cid=0b297fa8a249464ba34f5861d4140e58'
-
 var songs = JSON.parse(fs.readFileSync('./songs/songs.json', 'utf8'));
 
 const getRandomSong = () => {
@@ -19,14 +17,6 @@ const getRandomSong = () => {
 };
 
 app.use(cors());
-
-app.get('/stream', (req, res) => {
-  // request(URL, function(error, response, body) {
-  //   const songUrl = JSON.parse(body).media.transcodings.filter(transcoding => transcoding.format.protocol === 'progressive')[0].url;
-  //   res.json({ url: songUrl });
-  // });
-  res.json({ url: URL})
-});
 
 app.get('/allsongs', (req, res) => {
   res.json(songs);
